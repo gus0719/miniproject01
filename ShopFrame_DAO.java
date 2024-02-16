c5105def7c3b5b427cbddeb63f6ab305c50b44d9
gus0719 <skatkdgus071@gmail.com>
2024-02-16 23:05:11 +0900

    Filter String

--- ShopFrame_DAO.java
+++ ShopFrame_DAO.java
@@ -67,7 +67,7 @@ public class ShopFrame_DAO{
 	void printCategory() {
 		// 카테고리 화면
 		while(true) {
-			System.out.printf("\t+%1$s+\n\t|\t카테고리\t|\n\t+%1$s+\n", api.mLineReturn('-', 23));
+			System.out.printf("\t+%1$s+\n\t|\t카테고리\t\t|\n\t+%1$s+\n", api.mLineReturn('-', 23));
 			System.out.println("\t1.\t가구\n");// 카테고리 출력
 			System.out.println("\t2.\t가전디지털\n");// 카테고리 출력
 			System.out.println("\t3.\t식품\n");// 카테고리 출력
@@ -83,6 +83,10 @@ public class ShopFrame_DAO{
 			if(categoryRead.equalsIgnoreCase("Q")) {
 				System.out.println("메인 메뉴");
 				return;
+			}
+			if(categoryRead.length() >= 1 && (categoryRead.charAt(0) < '1' || categoryRead.charAt(0) > '9')){	// 숫자 이외의 문자 입력
+				System.out.println("\t^ 메뉴를 선택하세요");
+				continue;
 			}
 			if(categoryRead.length() > 0 && (Integer.parseInt(categoryRead) >= 1 && Integer.parseInt(categoryRead) <= 4)) {
 				// 카테고리 상품메뉴로 이동
@@ -120,6 +124,9 @@ public class ShopFrame_DAO{
 		System.out.printf("등록 일자 : %s\n", productDTOobj.getUploadDate());
 		System.out.printf("상품 설명\n%s\n", productDTOobj.getExplain());
 		api.mLine('-',80);
+		System.out.println("-> 1. 구매");
+		System.out.println("-> 2. 장바구니");
+		System.out.println("-> 3. 리뷰");
 		while(true){
 			System.out.print("구매, 장바구니, 리뷰를 선택하세요 [뒤로가기 : Q] : ");
 			String explainRead = scan.nextLine();
@@ -130,6 +137,23 @@ public class ShopFrame_DAO{
 			if(explainRead.equalsIgnoreCase("Q")) {
 				System.out.println("\t← 상품 이동");
 				return;
+			}
+			if(explainRead.length() >= 1 && explainRead.charAt(0) < '1' || explainRead.charAt(0) > '9'){	// 숫자 이외의 문자 입력
+				System.out.println("\t^ 메뉴를 선택하세요");
+				continue;
+			}
+			if(Integer.parseInt(explainRead) >= 1 && Integer.parseInt(explainRead) <= 3) {
+				switch(Integer.parseInt(explainRead)) {
+					case 1:
+						System.out.println("\t\t^구매 Algorithm");
+						break;
+					case 2:
+						System.out.println("\t\t^ 장바구니 Algorithm");
+						break;
+					case 3:
+						System.out.println("\t\t^ 리뷰 출력 Algorithm");
+						break;
+				}
 			}
 		}
 	}
@@ -154,6 +178,10 @@ public class ShopFrame_DAO{
 				System.out.println("\t← 카테고리 이동");
 				return;
 			}
+			if(productRead.length() >= 1 && (productRead.charAt(0) < '1' || productRead.charAt(0) > '9')){	// 숫자 이외의 문자 입력
+				System.out.println("\t^ 메뉴를 선택하세요");
+				continue;
+			}
 			if(productRead.length() <= productDTOobj.length && 
 					(Integer.parseInt(productRead) >= 1 && Integer.parseInt(productRead) <= productDTOobj.length)) {
 				// 상품 선택시 상품에 대한 정보 출력과 메뉴창이 출력됨
@@ -168,8 +196,9 @@ public class ShopFrame_DAO{
 		api.mLine('-', 30);
 		System.out.println("1. 카테고리");
 		System.out.println("2. 내 장바구니");
-		System.out.println("3. 내가 찜한 상품");
-		System.out.println("4. 내 정보 출력\n");
+		System.out.println("3. 내 정보 출력");
+		System.out.println("4. 로그인");
+		System.out.println("5. 회원가입\n");
 		System.out.println("EXIT. 종료");
 		api.mLine('-', 30);
 		System.out.print("메뉴를 선택하세요 [종료 : EXIT] : ");
@@ -187,20 +216,27 @@ public class ShopFrame_DAO{
 				System.out.println("시스템 종료합니다.");
 				System.exit(0);
 			}
-			if(choiceMenu.length() == 1 && (choiceMenu.charAt(0) >= '1' && choiceMenu.charAt(0) <= '4')) {
+			if(choiceMenu.length() >= 1 && (choiceMenu.charAt(0) < '1' || choiceMenu.charAt(0) > '9')){	// 숫자 이외의 문자 입력
+				System.out.println("\t^ 메뉴를 선택하세요");
+				continue;
+			}
+			if(choiceMenu.length() == 1 && (choiceMenu.charAt(0) >= '1' && choiceMenu.charAt(0) <= '5')) {
 				switch(Integer.parseInt(choiceMenu)) {
 					case 1:
 						System.out.println("\t카테고리 이동↘");
 						printCategory();
 						break;
 					case 2:
-						System.out.println("\\t내 장바구니 이동↘");
+						System.out.println("\t내 장바구니 이동 Algorithm↘");
 						break;
 					case 3:
-						System.out.println("\\t내가 찜한 상품 목록으로 이동↘");
+						System.out.println("내 정보 출력 Algorithm");
 						break;
 					case 4:
-						System.out.println("내 정보 출력 Algorithm");
+						System.out.println("로그인 Algorithm");
+						break;
+					case 5:
+						System.out.println("회원가입 Algorithm");
 						break;
 				}
 			}
